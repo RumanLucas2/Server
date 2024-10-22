1bddb54964684a005e717409448b82cfbffb1c5a
RumanLucas2 <lucasruman2@gmail.com>
2024-10-22 16:24:23 -0300

    Servidor Reup

--- src/main/java/org/example/Servidor.java
+++ src/main/java/org/example/Servidor.java
@@ -3,59 +3,109 @@ package org.example;
 import java.util.ArrayList;
 
 public class Servidor {
-    public static String PORTA_PADRAO = "8000";
+    private static boolean status = false;
+    public static class Inicializador{
+        private static String porta;
+        private static String[] args;
 
-    public static void main(String[] args) {
-        if (args.length > 1) {
-            System.err.println("Uso esperado: java Servidor [PORTA]\n");
-            return;
-        }
-
-        String porta = Servidor.PORTA_PADRAO;
-
-        if (args.length == 1)
-            porta = args[0];
-
-        ArrayList<Parceiro> usuarios = new ArrayList<>();
-
-        AceitadoraDeConexao aceitadoraDeConexao = null;
-        try {
-            aceitadoraDeConexao = new AceitadoraDeConexao(porta, usuarios);
-            aceitadoraDeConexao.start();
-        } catch (Exception erro) {
-            System.err.println("Escolha uma porta apropriada e liberada para uso!\n");
-            return;
-        }
-
-        for (; ; ) {
-            System.out.println("O servidor está ativo! Para desativá-lo,");
-            System.out.println("use o comando \"desativar\"\n");
-            System.out.print("> ");
-
-            String comando = null;
+        private static final ArrayList<Parceiro> usuarios = new ArrayList<>();
+        private static void Starter(String port, String[] arguments) throws InterruptedException {
+            porta = port;
+            args = arguments;
             try {
-                comando = Teclado.getUmString();
-            } catch (Exception erro) {
+                Start();
+
+            }catch (Exception ignore){}
+        }
+        private static void Start() throws InterruptedException {
+            if (args.length > 1) {
+                System.err.println("Uso esperado: java Servidor [PORTA]\n");
+                return;
             }
+            System.out.println("Servidor-> Porta Valido\n");
 
-            if (comando.toLowerCase().equals("desativar")) {
-                synchronized (usuarios) {
-                    ComunicadoDeDesligamento comunicadoDeDesligamento =
-                            new ComunicadoDeDesligamento();
+            if (args.length == 1)
+                porta = args[0];
 
-                    for (Parceiro usuario : usuarios) {
-                        try {
-                            usuario.receba(comunicadoDeDesligamento);
-                            usuario.adeus();
-                        } catch (Exception erro) {
-                        }
+            AceitadoraDeConexao aceitadoraDeConexao = null;
+            try {
+                aceitadoraDeConexao = new AceitadoraDeConexao(porta, usuarios);
+                aceitadoraDeConexao.start();
+                status = true;
+            } catch (Exception erro) {
+                System.err.println("Escolha uma porta apropriada e liberada para uso!\n");
+                return;
+            }
+            System.out.println("Porta Valida\n");
+            try {
+                Servidor.TestCmds();
+
+            }catch (Exception ignore){
+
+            }
+        }
+
+        public static void Terminate(){
+            synchronized (usuarios) {
+                ComunicadoDeDesligamento comunicadoDeDesligamento =
+                        new ComunicadoDeDesligamento();
+
+                for (Parceiro usuario : usuarios) {
+                    try {
+                        usuario.receba(comunicadoDeDesligamento);
+                        usuario.adeus();
+                    } catch (Exception ignored) {
                     }
                 }
-
-                System.out.println("O servidor foi desativado!\n");
                 System.exit(0);
-            } else
-                System.err.println("Comando inválido!\n");
+            }
         }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        // Inicializa algo de forma assíncrona
+        String PORTA_PADRAO = "8080";
+        Inicializador.Starter(PORTA_PADRAO, args);
+    }
+
+    private static final class cmd{
+        public enum GetCmd
+        {
+            Status {
+                @Override
+                public String execute(){
+                    return ""+Servidor.status;
+                }
+            },
+            Get(){
+                @Override
+                public String execute(){
+                    return "This is "+Boolean.TRUE;
+                }
+            }
+            ;
+
+            public abstract String execute();
+        }
+
+        public enum PostCmd
+        {
+            Terminate{
+                @Override
+                public void execute(){
+                    Inicializador.Terminate();
+                }
+            };
+            public abstract void execute();
+        }
+        public static String Get(cmd.GetCmd a){
+            return a.execute();
+        }
+        public static void Post(cmd.PostCmd a){
+            a.execute();
+        }
+    }
+    public static void TestCmds(){
+        System.out.println(cmd.Get(cmd.GetCmd.Get));
+        cmd.Post((cmd.PostCmd.Terminate));
     }
 }
