69dc044a3ca5683df8b83f6ebc56d12286c8ce6a
RumanLucas2 <lucasruman2@gmail.com>
2024-12-09 08:25:43 -0300

    Update Cliente.java

--- src/main/java/org/example/Cliente.java
+++ src/main/java/org/example/Cliente.java
@@ -42,42 +42,20 @@ public class Cliente {
             return;
         }
 
-//        try {
-//            String host = Cliente.HOST_PADRAO;
-//            int porta = Cliente.PORTA_PADRAO+1;
-//
-//            if (args.length > 0)
-//                host = args[0];
-//
-//            if (args.length == 2)
-//                porta = Integer.parseInt(args[1]);
-//
-//            //conn = new Socket(host, porta+1);
-//
-//        } catch (Exception erro) {
-//            System.err.println("Indique o servidor e a porta corretos em conn!\n");
-//            return;
-//        }
-
 
 
         try {
             servidor = new Parceiro(conexao, new ObjectInputStream(conexao.getInputStream()),  new ObjectOutputStream(conexao.getOutputStream())); //apenas recebe
-            //saida = new Parceiro(conn, new ObjectInputStream(conn.getInputStream()), new ObjectOutputStream(conn.getOutputStream()));// apenas envia
-
         } catch (Exception erro) {
             System.err.println("Indique o servidor e a porta corretos!\n");
             return;
         }
         try {
             while (conexao.isConnected()) {
-                // recebendo automaticamente dados do (sensor)
                 String command = servidor.receba();
 
                 if (command.equals("status")) {
-                    servidor.envie("Online");
-                    //saida.envie("Online");
-                } else {
+                    servidor.envie("Online");} else {
                     TryServer(command);
                     TryDB(command);
                 }
@@ -87,34 +65,29 @@ public class Cliente {
         }
     }
 
-    private static boolean TryServer(String command){
+    private static void TryServer(String command){
         String cmd = command.replace("Server ", "");
         try{
             String response = ServerCommand.valueOf(cmd).execute().toString();
             System.out.println(response);
             servidor.envie(response+" ");
-            return true;
         }catch(Exception e) {
             System.out.println("Server command not valid: " +cmd);
-            return false;
         }
     }
 
-    private static boolean TryDB(String cmd){
+    private static void TryDB(String cmd){
         if(DB.exists(cmd.split(" ")[0].trim())){
             try {
                 String response = DB.valueOf(cmd.split(" ")[0]).execute(cmd.replace(cmd.split(" ")[0], "")).toString();
                 response = response.trim();
                 System.out.println(response);
                 servidor.envie(response+" ");
-                return true;
             }catch (Exception ex){
                 System.out.println(ex.getMessage());
-                return true;
             }
         }else{
             System.err.println("DataBase command not valid: " +cmd);
-            return false;
         }
     }
 }
